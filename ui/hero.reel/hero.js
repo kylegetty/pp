6c0ee138e5a9a35aaf5f9b51318919726cb57803
Kyle <uokylegetty@gmail.com>
2016-01-26 05:29:02 +0000

    Add component hero

---
+++ ui/hero.reel/hero.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/hero.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Hero
+ * @extends Component
+ */
+exports.Hero = Component.specialize(/** @lends Hero# */ {
+    constructor: {
+        value: function Hero() {
+            this.super();
+        }
+    }
+});
